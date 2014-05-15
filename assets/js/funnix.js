48da040dc0e209a27037e2876cb87b38ae23f160
Nelson Antonino Paez <nelpa90@gmail.com>
2014-05-15 17:57:48 -0400

    Galeria para posts

--- assets/js/funnix.js
+++ assets/js/funnix.js
@@ -15,9 +15,36 @@ function crearGalerias(){
             $('body').data('galerias',++galeriaId);
         }
         var sig = $(img).next();
+        if(!sig.length){
+            sig = $(img).parent().next().children().first();
+        }
         if(sig.is('img')){
             $(img).addClass('galeria_'+galeriaId);
             $(sig).addClass('galeria_'+galeriaId);
         }
     });
+
+    for(var i=0;i<=$('body').data('galerias');i++){
+        $('.galeria_'+i).first().after(
+            $('<div>').attr('id','galeria_wrap_'+i)
+                .addClass('camera_wrap camera_azure_skin')
+        );
+
+        $('.galeria_'+i).each(function(index,img){
+            var divImg = $('<div>').attr('data-thumb',$(img).attr('src'))
+                .attr('data-src',$(img).attr('src'))
+                .appendTo('#galeria_wrap_'+i);
+
+            $('<div>').addClass('camera_caption fadeFromBottom')
+                .html($(img).attr('alt'))
+                .appendTo(divImg);
+            
+            $(img).remove();
+        });
+
+        $('#galeria_wrap_'+i).camera({
+            thumbnails: true
+        });
+
+    }
 }
